d3e582b9602bde710831ede4d28323f72c693d6f
Zach Flathers <flatherz@oregonstate.edu>
2023-04-25 21:38:21 -0700

    Add emulator.h to test linting

--- emulator.h
+++ emulator.h
@@ -1,6 +1,10 @@
+#include <stdbool.h>
+#include <stdint.h>
+
 #define MEM_SIZE 65536
 
-typedef struct {
+typedef struct
+{
   // Registers
   uint8_t a;
   uint8_t b;
@@ -16,11 +20,12 @@ typedef struct {
   uint8_t flags;
 
   /*
-  Sign (S): Set if the result of an operation is negative (most significant bit is 1).
-  Zero (Z): Set if the result of an operation is zero.
-  Auxiliary Carry (AC): Set if there's a carry from bit 3 to bit 4 during an operation.
-  Parity (P): Set if the result of an operation has an even number of 1 bits.
-  Carry (CY): Set if there's a carry from the most significant bit after an operation.
+  Sign (S): Set if the result of an operation is negative (most significant bit
+  is 1). Zero (Z): Set if the result of an operation is zero. Auxiliary Carry
+  (AC): Set if there's a carry from bit 3 to bit 4 during an operation. Parity
+  (P): Set if the result of an operation has an even number of 1 bits. Carry
+  (CY): Set if there's a carry from the most significant bit after an
+  operation.
   */
 
   // Program Counter & Stack pointer
@@ -39,12 +44,12 @@ typedef struct {
 } i8080;
 
 // Funct prototypes
-void cpu_init(i8080 *cpu);
-uint8_t cpu_read_mem(i8080 *cpu, uint16_t address);
-void cpu_write_mem(i8080 *cpu, uint16_t address, uint8_t data);
-bool cpu_load_file(i8080 *cpu, const char *file_path, uint16_t address);
-void execute_instruction(i8080 *cpu, uint8_t opcode);
+void cpu_init (i8080 *cpu);
+uint8_t cpu_read_mem (i8080 *cpu, uint16_t address);
+void cpu_write_mem (i8080 *cpu, uint16_t address, uint8_t data);
+bool cpu_load_file (i8080 *cpu, const char *file_path, uint16_t address);
+void execute_instruction (i8080 *cpu, uint8_t opcode);
 
 // Prototypes for Flags
-void cpu_set_flag(i8080 *cpu, uint8_t flag, bool value);
-bool cpu_get_flag(i8080 *cpu, uint8_t flag);
+void cpu_set_flag (i8080 *cpu, uint8_t flag, bool value);
+bool cpu_get_flag (i8080 *cpu, uint8_t flag);
