8afcb717940f15fc5e4da3c43fca57cf60340a83
Austin Horton <hortonau@oregonstate.edu>
2023-05-29 13:52:37 -0700

    modified port out to remove unused cases

--- emulator.c
+++ emulator.c
@@ -1520,11 +1520,6 @@ port_out(i8080 *cpu, uint8_t port, uint8_t value)
       cpu->shift_lsb = cpu->shift_msb;
       cpu->shift_msb = value;
       break;
-    case 5:
-      // play sound 2 from sound bank
-      break;
-    case 6:
-      break;
     default:
       fprintf(stderr, "Error: Unknown OUT port %02x\n", port);
       break;
